cf1279f536848b3a2e8d0408ae36c263910fb89c
mert1996 <merttekin0@gmail.com>
2023-07-07 08:45:08 +0300

    yorum model is created

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from blog.models import KategoriModel, YazilarModel
+from blog.models import KategoriModel, YazilarModel, YorumModel
 
 class YazilarAdmin(admin.ModelAdmin):
     list_display = ('baslik', 'yazar','olusturulma_tarihi')
     search_fields = ("baslik", "yazar")
 
+class YorumAdmin(admin.ModelAdmin):
+    list_display = ("yazan", "olusturulma_tarihi","guncellenme_tarihi")
+    search_fields = ("yazan__username",)
+
 
 admin.site.register(KategoriModel)
 admin.site.register(YazilarModel,YazilarAdmin)
+admin.site.register(YorumModel, YorumAdmin)
