81ad56d6390405a426dafd8525010be40b1f5cb3
mert1996 <merttekin0@gmail.com>
2023-07-17 12:30:58 +0300

    kategori ve arama sayfası yapıldı.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from blog.views import iletisim, anasayfa
+from blog.views import iletisim, anasayfa, kategori, yazilarim
 
 
 urlpatterns = [
-    path("iletisim/", iletisim),
-    path("", anasayfa)
+    path("iletisim/", iletisim, name="iletisim"),
+    path("", anasayfa, name="anasayfa"),
+    path("kategori/<slug:kategoriSlug>", kategori, name="kategori"),
+    path("yazilarim", yazilarim, name="yazilarim")
 ]
--- blog/views/iletisim.py
+++ blog/views/iletisim.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 def iletisim(request):
     context = {
-        "isim":"iletişim sayfasi"
+        "key": "başlık içerik naber"
     }
     return render(request, "pages/iletisim.html", context=context)
